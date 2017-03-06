0cf3c96d6339905a1705712aaf1d367622322255
zerogtw <zerogtw@163.com>
2017-03-06 22:37:34 +0800

    iPad of Photomania
    New:
    1.Add iPad storyboard.
    2.Add something new controller to show the photo of special
    photographer.
    3.Make a universal App that can run on iPhone and iPad

---
+++ Photomania/Photomania/PhotographersCDTVC.m
@@ -0,0 +1,90 @@
+//
+//  PhotographersCDTVC.m
+//  Photomania
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2013 Stanford University. All rights reserved.
+//
+
+#import "PhotographersCDTVC.h"
+#import "Photographer.h"
+#import "PhotoDatabaseAvailability.h"
+#import "PhotoByPhotographerCDTVC.h"
+
+@implementation PhotographersCDTVC
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    [[NSNotificationCenter defaultCenter] addObserverForName:PhotoDatabaseAvailabilityNotification
+                                                      object:nil
+                                                       queue:nil
+                                                  usingBlock:^(NSNotification *note) {
+                                                      self.managedObjectContext = note.userInfo[PhotoDatabaseAvailabilityContext];
+                                                  }];
+}
+
+- (void)setManagedObjectContext:(NSManagedObjectContext *)managedObjectContext
+{
+    _managedObjectContext = managedObjectContext;
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Photographer"];
+    request.predicate = nil;
+    request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"name"
+                                                              ascending:YES
+                                                               selector:@selector(localizedStandardCompare:)]];
+    
+    self.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:request
+                                                                        managedObjectContext:managedObjectContext
+                                                                          sectionNameKeyPath:nil
+                                                                                   cacheName:nil];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"Photographer Cell"];
+    
+    Photographer *photographer = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    
+    cell.textLabel.text = photographer.name;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d photos", (int)[photographer.photos count]];
+    
+    return cell;
+}
+
+#pragma mark - Navigation
+
+- (void)prepareViewController:(id)vc forSegue:(NSString *)segueIdentifer fromIndexPath:(NSIndexPath *)indexPath
+{
+    Photographer *photographer = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    if ([vc isKindOfClass:[PhotoByPhotographerCDTVC class]]) {
+        PhotoByPhotographerCDTVC *pbpcdtvc = (PhotoByPhotographerCDTVC *)vc;
+        pbpcdtvc.photograpger = photographer;
+    }
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    NSIndexPath *indexPath = nil;
+    if ([sender isKindOfClass:[UITableViewCell class]]) {
+        indexPath = [self.tableView indexPathForCell:sender];
+    }
+    [self prepareViewController:segue.destinationViewController
+                       forSegue:segue.identifier
+                  fromIndexPath:indexPath];
+    NSLog(@"1");
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    id detailvc = [self.splitViewController.viewControllers lastObject];
+    if ([detailvc isKindOfClass:[UINavigationController class]]) {
+        detailvc = [((UINavigationController *)detailvc).viewControllers firstObject];
+        [self prepareViewController:detailvc
+                           forSegue:nil
+                      fromIndexPath:indexPath];
+    }
+    NSLog(@"2");
+}
+
+@end
